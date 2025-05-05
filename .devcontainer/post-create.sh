e4ebb61a164504d66c9309727c2ad9f86713a585
demouser <alex.thissen@xebia.com>
2025-05-05 14:26:27 +0000

    Assets for VSCode

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,6 +2,8 @@
 
 # This script is run after the container is created and the workspace is mounted.
 git config --global user.name 'demouser'
+git config --global user.email 'alex.thissen@xebia.com'
+git config --global core.autocrlf input
 
 #sudo dotnet workload update
 dotnet restore
